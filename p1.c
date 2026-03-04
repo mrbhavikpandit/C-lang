dbf616fab2e9e1da37a45d5c88b45cee413941c0
Pandit Bhavik <mrbhavikpandit@gmail.com>
2026-03-04 05:52:53 +0000

    Add basic string initialization program in p1.c

---
+++ p1.c
@@ -0,0 +1,7 @@
+//Write a program to declare a string initialize it and print it .
+#include <stdio.h>
+int main() {
+    char str[] = "Hello, World!";  
+    printf("%s\n", str);           
+    return 0;
+}
