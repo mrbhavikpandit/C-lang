21e1e1adbf85c9c494e78626696b25ed844e5528
Pandit Bhavik <mrbhavikpandit@gmail.com>
2026-03-04 05:55:47 +0000

    Add string input program p2.c

---
+++ p2.c
@@ -0,0 +1,10 @@
+//write a program to read a sting from user and display it
+#include <stdio.h>
+ int main()
+{
+    char str[100];
+    printf("Enter a string: ");
+    scanf("%s", str);
+     printf("The string you entered: %s\n", str);
+     return 0;
+}
