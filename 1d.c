5f4fef5a5f99863a23410503208ea38013744924
Pandit Bhavik <mrbhavikpandit@gmail.com>
2026-03-04 06:03:42 +0000

    Add string copy program 1d.c

---
+++ 1d.c
@@ -0,0 +1,12 @@
+// Implement a string function to copy one string to another.
+#include <stdio.h>
+#include <string.h>
+ int main()
+{
+    char str1[100], str2[100];
+     printf("Enter the first string: ");
+    scanf("%s", str1);
+    strcpy(str2, str1);
+     printf("Copied string: %s\n", str2);
+    return 0;
+}
