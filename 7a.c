f5e4c44f8d68d21cdb592d0e89639fede02ac925
Pandit Bhavik <mrbhavikpandit@gmail.com>
2026-03-12 16:09:14 +0530

    Add 7a.c

---
+++ 7a.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int *p, *q;
+    int i;
+
+    // malloc
+    p = (int*) malloc(5 * sizeof(int));
+
+    printf("Enter 5 numbers (malloc):\n");
+    for(i=0;i<5;i++)
+        scanf("%d",&p[i]);
+
+    printf("Numbers are:\n");
+    for(i=0;i<5;i++)
+        printf("%d ",p[i]);
+
+    free(p);   // free memory
+
+    // calloc
+    q = (int*) calloc(5, sizeof(int));
+
+    printf("\nEnter 5 numbers (calloc):\n");
+    for(i=0;i<5;i++)
+        scanf("%d",&q[i]);
+
+    printf("Numbers are:\n");
+    for(i=0;i<5;i++)
+        printf("%d ",q[i]);
+
+    free(q);   // free memory
+
+    return 0;
+}
