4fd242565db97ba92c2d7ebc180f613b3de24e68
Pandit Bhavik <mrbhavikpandit@gmail.com>
2026-03-12 14:01:01 +0530

    Add 3c.c

---
+++ 3c.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+
+void reverse(char str[], int i)
+{
+    if(str[i]=='\0')
+        return;
+
+    reverse(str, i+1);
+    printf("%c", str[i]);
+}
+
+int main()
+{
+    char str[100];
+
+    printf("Enter string: ");
+    scanf("%s",str);
+
+    reverse(str,0);
+
+    return 0;
+}
