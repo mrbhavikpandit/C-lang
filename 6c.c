837e173a4887fde1d169fa314acff06f5876b3a5
Pandit Bhavik <mrbhavikpandit@gmail.com>
2026-03-12 16:05:04 +0530

    Add 6c.c

---
+++ 6c.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    FILE *fp;
+    int pos;
+
+    fp = fopen("file.txt", "w+");
+
+    fprintf(fp,"Hello World");
+
+    pos = ftell(fp);
+    printf("Current position: %d\n", pos);
+
+    rewind(fp);
+    printf("Position after rewind: %d\n", ftell(fp));
+
+    fseek(fp, 6, SEEK_SET);
+    printf("Position after fseek: %d\n", ftell(fp));
+
+    fclose(fp);
+
+    return 0;
+}
