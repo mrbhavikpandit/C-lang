7f131feef9ea3c3bff8ad9a27ab617f4113335c8
Pandit Bhavik <mrbhavikpandit@gmail.com>
2026-03-12 15:41:36 +0530

    Add 4b.c

---
+++ 4b.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+struct book
+{
+    char title[50];
+    char author[50];
+    float price;
+};
+
+int main()
+{
+    struct book b;
+
+    printf("Enter title: ");
+    scanf("%s", b.title);
+    printf("Enter author: ");
+    scanf("%s", b.author);
+    printf("Enter price: ");
+    scanf("%f", &b.price);
+
+    printf("\nBook Details\n");
+    printf("Title: %s\n", b.title);
+    printf("Author: %s\n", b.author);
+    printf("Price: %.2f\n", b.price);
+
+    return 0;
+}
