d9c6a27939f99509225078375ddd6706476a1759
Pandit Bhavik <mrbhavikpandit@gmail.com>
2026-03-12 16:00:36 +0530

    Add 6b.c

---
+++ 6b.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    FILE *fp;
+    char name[50];
+    int age;
+
+    fp = fopen("data.txt", "w");
+
+    fprintf(fp, "John 21");   // writing data
+    fclose(fp);
+
+    fp = fopen("data.txt", "r");
+
+    fscanf(fp, "%s %d", name, &age);   // reading data
+
+    printf("Name: %s\nAge: %d", name, age);
+
+    fclose(fp);
+
+    return 0;
+}
